a2766fd69edaf16f9ec60900078021d28af1047e
chadlaban <laban.chad@gmail.com>
2023-06-01 15:01:27 +0800

    Importing custom CSS file.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './assets/css/custom.css';
 
 createApp(App).mount('#app')
