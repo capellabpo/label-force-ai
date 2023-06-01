8ba4e11b11cfc522905d70603538a5cae50a649e
chadlaban <laban.chad@gmail.com>
2023-06-01 16:17:18 +0800

    Importing router view pages.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,47 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './assets/css/custom.css';
 
-createApp(App).mount('#app')
+import LoginView from "./views/LoginView.vue";
+import ForgotPasswordView from "./views/ForgotPasswordView.vue";
+import RegisterView from "./views/RegisterView.vue";
+import EmailVerificationView from "./views/EmailVerificationView.vue";
+import EmailOtpCodeView from "./views/EmailOtpCodeView.vue";
+import AccountCreated from "./views/AccountCreated.vue";
+
+const app = createApp(App);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/login",
+      component: LoginView,
+    },
+    {
+        path: "/forgotpassword",
+        component: ForgotPasswordView,
+    },
+    {
+        path: "/signup",
+        component: RegisterView,
+    },
+    {
+        path: "/emailverification",
+        component: EmailVerificationView,
+    },
+    {
+        path: "/otpverification",
+        component: EmailOtpCodeView,
+    },
+    {
+        path: "/registered",
+        component: AccountCreated,
+    }
+  ],
+});
+
+app.use(router);
+app.mount('#app')
