4442b582045cf5f0331f3eb5190eff2d634f03d0
chadlaban <laban.chad@gmail.com>
2023-06-01 17:02:16 +0800

    Converting single file vue to router vue pages.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <LoginView></LoginView>
+  <div class="container">
+    <div id="brand-col">
+      <div class="app-title">
+        <img class="label-force-logo" src="./assets/imgs/Lb_logo.png" />
+      </div>
+      <div class="app-branding">
+        <img class="branding-img" src="./assets/imgs/Lb_gfx.png" />
+      </div>
+    </div>
+
+
+    <div class="divider"></div>
+
+    <!-- Use desktop versions for now -->
+    <!-- Work on actual sign-in -->
+    <div id="sign-in-col">
+      <router-view></router-view>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import LoginView from './components/LoginView.vue'
-
-export default {
-  components: {
-    LoginView
-  }
-}
 </script>
 
-<style>
-</style>
+<style></style>
